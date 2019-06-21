ee668f09f337c184428c931984c234519bbf90fb
teravice <teravice@gmail.com>
2019-06-21 10:44:12 +0100

    Update pywordlistgenerator.py

--- pywordlistgenerator.py
+++ pywordlistgenerator.py
@@ -14,24 +14,27 @@ upl = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 numb = '1234567890'
 spch = '`~!@#$%^&*()_+=-[]{}<>,./?"\|'
 ch = ''
+flnm = ''
 
-question = input("Do you want lowercase letters? [Y/n] > ")
+question = raw_input("Do you want lowercase letters? [Y/n] > ")
 if question=='y' or question=='':
-	ch+=lwl
-question = input("Do you want uppercase letters? [Y/n] > ")
+        ch+=lwl
+question = raw_input("Do you want uppercase letters? [Y/n] > ")
 if question=='y' or question=='':
-	ch+=upl
-question = input("Do you want numbers? [Y/n] > ")
+        ch+=upl
+question = raw_input("Do you want numbers? [Y/n] > ")
 if question=='y' or question=='':
-	ch+=numb
-question = input("Do you want special characters? [Y/n] > ")
+        ch+=numb
+question = raw_input("Do you want special characters? [Y/n] > ")
 if question=='y' or question=='':
-	ch+=spch
-rep = input("Enter number of characters e.g. 3 > ")
+        ch+=spch
+rep = raw_input("Enter number of characters e.g. 3 > ")
 
-question = input("Type file name or enter for default(dict.txt) > ")
+question = raw_input("Type file name or enter for default(dict.txt) > ")
 if question=='':
-	flnm="dict.txt"
+        flnm="dict.txt"
+else:
+        flnm=question
 
 f=open(flnm, 'w')
 
@@ -39,9 +42,9 @@ print ("Depending on your selections, this may take a while.")
 print ("Please be patient...")
 
 for j in range(1,int(rep)+1):
-	res = itertools.product(ch, repeat=j) # 3 is the length of your result.
-	for i in res: 
-		f.write("".join(i)+"\n")
+        res = itertools.product(ch, repeat=j) # 3 is the length of your result.
+        for i in res: 
+                f.write("".join(i)+"\n")
 
 f.close()
 
